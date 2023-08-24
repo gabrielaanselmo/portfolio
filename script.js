b5e2c3ac73478a50824add3e78321af3f5ad47bb
Maria Gabriela <gabrielaanselmao@gmail.com>
2023-08-24 11:46:16 -0300

    Adicionando efeito de deslizamento com o scroll

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function isElementVisible(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    (rect.top <= window.innerHeight && rect.bottom >= 0) ||
+    (rect.bottom >= 0 && rect.top <= 0)
+  );
+}
+
+function handleScroll() {
+  const elementsToShow = document.querySelectorAll('.hidden');
+
+  elementsToShow.forEach(element => {
+    if (isElementVisible(element)) {
+      element.classList.add('show');
+    } else {
+      element.classList.remove('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
+
+document.addEventListener('DOMContentLoaded', function() {
+  handleScroll();
+});
+
